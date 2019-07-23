018d4e0f0be8ad46b345d55f5b8556ab07a5929d
Robin Rijkeboer <robin.rijkeboer@itonomy.nl>
2019-07-23 16:21:59 +0200

    Change permissions to be more lenient

--- lib/capistrano/magento/defaults.rb
+++ lib/capistrano/magento/defaults.rb
@@ -23,7 +23,7 @@ set :magento_deploy_confirm, fetch(:magento_deploy_confirm, [])
 set :magento_deploy_maintenance, fetch(:magento_deploy_maintenance, true)
 
 # deploy permissions defaults
-set :magento_deploy_chmod_d, fetch(:magento_deploy_chmod_d, '0500')
-set :magento_deploy_chmod_f, fetch(:magento_deploy_chmod_f, '0400')
-set :magento_deploy_var_media_chmod_d, fetch(:magento_deploy_var_media_chmod_d, '0700')
-set :magento_deploy_var_media_chmod_f, fetch(:magento_deploy_var_media_chmod_f, '0600')
+set :magento_deploy_chmod_d, fetch(:magento_deploy_chmod_d, '0740')
+set :magento_deploy_chmod_f, fetch(:magento_deploy_chmod_f, '0640')
+set :magento_deploy_var_media_chmod_d, fetch(:magento_deploy_var_media_chmod_d, '0740')
+set :magento_deploy_var_media_chmod_f, fetch(:magento_deploy_var_media_chmod_f, '0640')
